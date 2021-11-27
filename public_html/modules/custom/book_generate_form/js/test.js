8ba89e92dddbafe26da9d2bcef36b4e6c232de26
te1m0z <dmitriy.balamozhnov@gmail.com>
2021-11-27 23:05:43 +0300

    Book-generate module fixes & added css to this module

--- public_html/modules/custom/book_generate_form/js/test.js
+++ public_html/modules/custom/book_generate_form/js/test.js
@@ -1,16 +1,33 @@
 (function ($) {
     $(document).ready(function () {
-        $('#submit-form-btn').click(function () {
-            $('#form-result-input').val($('#form-book').val())
-            $('#result-text').html($('#form-book').val())
+        $('#display-stroke-form-btn').click(function (event) {
+            event.preventDefault()
+
+            let str = getBookStroke()
+
+            $('#form-result-input').val(str)
+            $('#result-text').html(str)
         })
 
         $('#form-result-input').on('input', function (event) {
             $('#result-text').html(event.target.value)
         })
-
-        $('#submit-form-btn').click(function (event) {
-            event.preventDefault()
-        })
     })
+
+    function getBookStroke() {
+        let author = $('#form-book-author').val()
+        let name = $('#form-book-name').val()
+        let tomeNum = $('#form-book-tome-num').val()
+        let tomeMax = $('#form-book-tome-max').val()
+        let tomeName = $('#form-book-tome-name').val()
+        let city = $('#form-book-city').val()
+        let publish = $('#form-book-publish').val()
+        let year = $('#form-book-year').val()
+        let pages = $('#form-book-pages').val()
+
+
+        let stroke = `<i>${author}</i> ${name}. ${city} : ${publish}, ${year}. ${pages} с.`
+
+        return stroke
+    }
 })(jQuery)
--- public_html/modules/custom/book_generate_form/src/Form/BookFormSettings.php
+++ public_html/modules/custom/book_generate_form/src/Form/BookFormSettings.php
@@ -6,7 +6,9 @@ use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\node\Entity\Node;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-
+// use Drupal\Core\Ajax;
+// use Drupal\Core\Ajax\AjaxResponse;
+// use Drupal\Core\Ajax\ReplaceCommand;
 
 class BookFormSettings extends FormBase
 {
@@ -15,55 +17,173 @@ class BookFormSettings extends FormBase
         return 'book-generate-form';
     }
 
+    public function check_e_version(array $form, FormStateInterface $form_state)
+    {
+        $this->messenger()->addStatus('test');
+        $form_state->setValueForElement($form['nav_field'], ['#value' => '1111']);
+
+        return $form;
+    }
+
     public function buildForm(array $form, FormStateInterface $form_state)
     {
 
+        // $form['nav_e-version_checkbox'] = [
+        //     '#type' => 'checkbox',
+        //     '#title' => 'У меня электронная версия',
+            // '#options' => [
+            //     '1' => 'Бумажный',
+            //     '2' => 'Электронный'
+            // ],
+        //     '#ajax' => [
+        //         'callback' => '::check_e_version',
+        //         // 'wrapper' => 'e_version_wrapper',
+        //         'event' => 'change'
+        //     ],
+        // ];
+
+        // $form['nav_field'] = [
+        //     '#type' => 'textfield',
+        //     '#title' => 'URL',
+        //     // '#placeholder' => 'https://example.com',
+        //     '#required' => true,
+        //     '#id' => 'form-book-url',
+        //     '#value' => 'da'
+        // ];
+
         $form['title'] = [
             '#type' => 'textfield',
-            '#title' => 'Название списка',
-            '#placeholder' => 'Толстой. Война и мир',
-            '#required' => false,
-        ];
-
-        $form['name'] = [
-            '#type' => 'textfield',
-            '#title' => $this->t('Book\'s title'),
-            '#placeholder' => $this->t('Имя книги'),
-            '#required' => true,
-            '#id' => 'form-book'
+            '#title' => 'Название списка (необязательно)',
+            '#placeholder' => 'Толстой. Война и мир'
         ];
 
         $form['author'] = [
-            '#type' => 'textfield',
-            '#title' => $this->t('Book\'s author'),
-            '#placeholder' => $this->t('Имя автора книги'),
-            '#required' => true
+            '#type' => 'textarea',
+            '#title' => 'Автор(ы)',
+            '#placeholder' => 'Иванов И. И.',
+            '#required' => true,
+            '#id' => 'form-book-author',
+            '#rows' => '2'
         ];
 
-        $form['submit'] = array(
+        $form['name'] = [
+            '#type' => 'textarea',
+            '#title' => 'Название книги',
+            '#placeholder' => 'Война и мир',
+            '#required' => true,
+            '#id' => 'form-book-name',
+            '#rows' => '2'
+        ];
+
+        $form['tome-num'] = [
+            '#type' => 'textfield',
+            '#attributes' => [
+                ' type' => 'number',
+            ],
+            '#title' => 'Номер тома',
+            '#placeholder' => '3',
+            '#required' => false,
+            '#id' => 'form-book-tome-num'
+        ];
+
+        $form['tome-max'] = [
+            '#type' => 'textfield',
+            '#attributes' => [
+                ' type' => 'number',
+            ],
+            '#title' => 'Всего томов',
+            '#placeholder' => '5',
+            '#required' => false,
+            '#id' => 'form-book-tome-max'
+        ];
+
+        $form['tome-name'] = [
+            '#type' => 'textfield',
+            '#title' => 'Название тома',
+            '#placeholder' => 'Том первый',
+            '#required' => false,
+            '#id' => 'form-book-tome-name'
+        ];
+
+        $form['city'] = [
+            '#type' => 'textfield',
+            '#title' => 'Место издания (город)',
+            '#placeholder' => 'Саратов',
+            '#required' => true,
+            '#id' => 'form-book-city'
+        ];
+
+        $form['publish'] = [
+            '#type' => 'textfield',
+            '#title' => 'Издательство',
+            '#placeholder' => 'Наука',
+            '#required' => true,
+            '#id' => 'form-book-publish'
+        ];
+
+        $form['year'] = [
+            '#type' => 'textfield',
+            '#attributes' => [
+                ' type' => 'number'
+            ],
+            '#title' => 'Год издания',
+            '#placeholder' => '2018',
+            '#required' => true,
+            '#id' => 'form-book-year'
+        ];
+
+        $form['pages'] = [
+            '#type' => 'textfield',
+            '#attributes' => [
+                ' type' => 'number'
+            ],
+            '#title' => 'Количество страниц',
+            '#placeholder' => '240',
+            '#required' => true,
+            '#id' => 'form-book-pages'
+        ];
+
+        // $form['e-version'] = [
+        //     '#type' => 'textfield',
+        //     '#title' => 'URL',
+        //     '#placeholder' => 'https://example.com',
+        //     '#required' => true,
+        //     '#id' => 'form-book-url'
+        // ];
+
+
+
+
+
+
+
+
+        $form['display_result'] = [
             '#type' => 'button',
-            '#value' => $this->t('Готово'),
-            '#button_type' => 'primary',
-            '#id' => 'submit-form-btn'
-        );
+            '#value' => 'Посмотреть результат',
+            '#id' => 'display-stroke-form-btn',
+        ];
 
         $form['result_text'] = [
-            '#markup' => '<div id="result-text">' . $form_state->getValue('result_input') . '</div>',
-            '#prefix' => '<p>' . $this->t('Результат:') . '</p>'
+            '#type' => 'item',
+            '#title' => 'Результат:',
+            '#markup' => '<div id="result-text"></div>',
         ];
 
         $form['result_input'] = [
             '#type' => 'textarea',
-            '#title' => $this->t('Редактировать:'),
-            '#id' => 'form-result-input'
+            '#title' => 'Редактировать:',
+            '#id' => 'form-result-input',
+            '#rows' => '2',
+        ];
+
+        $form['submit'] = [
+            '#type' => 'submit',
+            '#value' => \Drupal::currentUser()->isAuthenticated() ? 'Сохранить список' : 'Войдите чтобы сохранить',
+            '#button_type' => 'primary',
         ];
 
         $form['#attached']['library'][] = 'book_generate_form/book_generate_form';
-
-        $form['save_list'] = array(
-            '#type' => 'submit',
-            '#value' => \Drupal::currentUser()->isAuthenticated() ? 'Сохранить список' : 'Войдите чтобы сохранить',
-        );
 
         return $form;
     }
@@ -72,10 +192,12 @@ class BookFormSettings extends FormBase
     {
         $form_state->setRebuild(true);
 
+        $title_val = $form_state->getValue('title');
+
         if (\Drupal::currentUser()->isAuthenticated()) {
             $node = Node::create(['type' => 'article']);
-            $node->setTitle($form_state->getValue('title'));
-            $node->body->value = $form_state->getValue('name');
+            $node->setTitle($title_val !== '' ? $title_val : 'Список без заголовка');
+            $node->body->value = $form_state->getValue('result_input');
             $node->body->format = 'full_html';
             $node->field_type->value = 'Книга';
             $node->setPublished(true);
